a86c1f000cb0ed4e017ffdf6cfaed22a5768f437
Michael Clark-Cuadrado <michael@michaelclarkcuadrado.com>
2018-12-20 02:27:32 -0500

    Add theme to vuetify

--- application/src/frontend/js/index.js
+++ application/src/frontend/js/index.js
@@ -1,7 +1,17 @@
 import Vue from 'vue';
 import 'vuetify/dist/vuetify.min.css';
 import Vuetify from 'vuetify';
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+    theme: {
+        primary: "#455A64",
+        secondary: "#607D8B",
+        accent: "#0288D1",
+        error: "#f44336",
+        warning: "#ffeb3b",
+        info: "#2196f3",
+        success: "#4caf50"
+    },
+});
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 import PropertySummaryView from './components/PropertySummaryView.vue';
@@ -9,5 +19,5 @@ import PropertySummaryView from './components/PropertySummaryView.vue';
 
 const vue = new Vue({
     el: '#app',
-    components: {PropertySummaryView}
+    components: { PropertySummaryView }
 });
