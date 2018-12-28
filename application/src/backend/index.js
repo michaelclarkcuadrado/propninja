b741195c55afebd1de1200eae3855ec0111a9326
michaelclarkcuadrado <michael@michaelclarkcuadrado.com>
2018-12-28 00:11:01 -0500

    Sort Stages on cards by rdering criteria

--- application/src/backend/index.js
+++ application/src/backend/index.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
-//TODO change for production
 const port = 8888;
 const dbHostname = "database-server";
 const dbUser = "root";
@@ -19,7 +18,7 @@ const dbConnectionPool = mysql.createPool({
 });
 
 //serve static assets - webpack'd and copied in by docker
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.redirect('static');
 });
 app.use('/static', express.static('static'));
@@ -29,40 +28,40 @@ app.use('/static', express.static('static'));
 /*
   Returns a list of all the stages that a unit can possibly be in.
 */
-app.get('/getKanbanStages', function(req, res){
-    //TODO take out. just for development
+app.get('/getKanbanStages', function (req, res) {
+  //TODO take out. just for development
   res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
-    dbConnectionPool.query("SELECT * FROM stages ORDER BY ordering_ID", function(error, results, fields){
-      let stagesObj = {};
-      for(let i = 0; i < results.length; i++){
-        stagesObj[results[i].stage_ID] = results[i];
-      }
-      res.json(stagesObj);
-    });
+  dbConnectionPool.query("SELECT * FROM stages ORDER BY ordering_ID", function (error, results, fields) {
+    let stagesObj = {};
+    for (let i = 0; i < results.length; i++) {
+      stagesObj[results[i].stage_ID] = results[i];
+    }
+    res.json(stagesObj);
+  });
 });
 
 /*
   Returns summaryObjs that are consumed by PropertySummaryCards. Bird's eye view of property data
   possible options are: lastSeen for pagination, ordering, inverted(asc to dsc), and/or a search term sent by GET params
 */
-app.get('/getPropertySummaries', function(req, res){
+app.get('/getPropertySummaries', function (req, res) {
   //TODO take out. just for development
   res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
   console.log(req.query);
 
-  if(Object.keys(req.query).length !== 4){
+  if (Object.keys(req.query).length !== 4) {
     throw new Error("Required parameters not received.");
   }
-  
+
   let sortingModes = [ // this must be kept in sync with 'supported modes' in PropertySummaryView
     'num_Units',
     'current_Value',
     'current_Debt',
     'currentRentRoll'
   ];
-  
-  let sqlQuery = 
-  `SELECT prop_ID, 
+
+  let sqlQuery =
+    `SELECT prop_ID, 
          prop_address, 
          current_Debt, 
          current_Value, 
@@ -82,32 +81,41 @@ app.get('/getPropertySummaries', function(req, res){
     (sortingModes[req.query.ordering]),
     Number(req.query.lastSeen),
   ];
-  dbConnectionPool.query(sqlQuery, sqlParams, function (error, results, fields) {
+  dbConnectionPool.query(sqlQuery, sqlParams, function (error, propertyResults) {
     if (error) {
       throw error;
     }
-    //collate rows into objects with a unified 'units' property. 
-    //Group_concat was necessary to be able to sort entries on database side
-    for (let i = 0; i < results.length; i++) {
-      let unitsObjs = {};
-      if (results[i].unit_Stages !== null) {
-        results[i].unit_Stages.split(',').forEach(function (stageID) {
-          if (stageID in unitsObjs) {
-            unitsObjs[stageID].num++;
-          } else {
-            unitsObjs[stageID] = { num: 1, stageID: stageID }
-          }
-        });
+    //get mapping of stageID to ordering ID to sort 
+    dbConnectionPool.query("SELECT stage_ID, ordering_ID from stages", function (error, stageResults) {
+      if (error) {
+        throw error;
       }
-      results[i]['units'] = Object.values(unitsObjs)
-      delete results[i].unit_IDs;
-      delete results[i].unit_Stages;
-    }
-    res.json(results);
+      let stageMap = {};
+      for(row in stageResults){
+        stageMap[stageResults[row].stage_ID] = stageResults[row].ordering_ID;
+      }
+      //collate rows into objects with a unified 'units' property. 
+      //Group_concat was necessary to be able to sort entries on database side
+      for (let i = 0; i < propertyResults.length; i++) {
+        let unitsObjs = {};
+        if (propertyResults[i].unit_Stages !== null) {
+          propertyResults[i].unit_Stages.split(',').forEach(function (stageID) {
+            if (stageID in unitsObjs) {
+              unitsObjs[stageID].num++;
+            } else {
+              unitsObjs[stageID] = { num: 1, stageID: stageID }
+            }
+          });
+        }
+        propertyResults[i]['units'] = Object.values(unitsObjs);
+        propertyResults[i]['units'].sort((a,b) => stageMap[a.stageID] - stageMap[b.stageID]);
+        delete propertyResults[i].unit_IDs;
+        delete propertyResults[i].unit_Stages;
+      }
+      res.json(propertyResults);
+    });
   });
 });
-
-
 
 //start server
 app.listen(port);
